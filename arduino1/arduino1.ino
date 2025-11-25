3cc59c78ff8afab6e170e1cd9a205aaeb208b218
Toan Nguyen Vo Khanh <khanhtoandn99@Toans-MacBook-Air.local>
2025-11-26 00:33:48 +0700

    DHT task ok

--- arduino1/arduino1.ino
+++ arduino1/arduino1.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
 #include <Keypad.h>
+#include <DHT11.h>
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>
 
@@ -8,13 +9,13 @@
 #define PIN_LDR_SENSOR2 A1
 // Write pins
 // PWM pins
-#define PIN_SERVO_SOLAR_TRACKER 10
+#define PIN_SERVO_DOOR 10
+#define PIN_SERVO_SOLAR_TRACKER 11
+// Digital Communication pins
+#define PIN_DHT11 12
 
 #define STATE_SUNNY LOW
 #define STATE_NO_SUNNY HIGH
-
-Servo servoSolarTracker;
-int servoSolarTrackerPos = 90;
 
 const byte KPAD_ROWS_MAX = 4;
 const byte KPAD_COLS_MAX = 4;
@@ -26,8 +27,31 @@ char keys[KPAD_ROWS_MAX][KPAD_COLS_MAX] = {
 };
 byte kpadRowPins[KPAD_ROWS_MAX] = {9,8,7,6};
 byte kpadColPins[KPAD_COLS_MAX] = {5,4,3,2};
-
 Keypad kpad = Keypad( makeKeymap(keys), kpadRowPins, kpadColPins, KPAD_ROWS_MAX, KPAD_COLS_MAX );
+
+DHT11 dht11(PIN_DHT11);
+
+Servo servoDoor;
+
+Servo servoSolarTracker;
+int servoSolarTrackerPos = 90;
+
+void openDoor();
+void closeDoor();
+
+void openDoor() {
+  for (int pos = 0; pos <= 90; pos += 1) {
+    servoDoor.write(pos);
+    vTaskDelay(20 / portTICK_PERIOD_MS);
+  }
+}
+
+void closeDoor() {
+  for (int pos = 90; pos > 0; pos -= 1) {
+    servoDoor.write(pos);
+    vTaskDelay(20 / portTICK_PERIOD_MS);
+  }
+}
 
 
 void TaskSolarTrackingController(void *pvParameters) {
@@ -74,12 +98,34 @@ void TaskDoorLockController(void *pvParameters) {
   }
 }
 
+void TaskTempHumiReaderController(void *pvParameters) {
+  Serial.println("TaskTempHumiReaderController Started!");
+  for (;;)
+  {
+    int temperature = 0;
+    int humidity = 0;
+    int result = dht11.readTemperatureHumidity(temperature, humidity);
+    if (result == 0) {
+        Serial.print("Temperature: ");
+        Serial.print(temperature);
+        Serial.print(" °C\tHumidity: ");
+        Serial.print(humidity);
+        Serial.println(" %");
+    } else {
+        Serial.println(DHT11::getErrorString(result));
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
   pinMode(PIN_LDR_SENSOR1, INPUT);
   pinMode(PIN_LDR_SENSOR2, INPUT);
+
+  servoDoor.attach(PIN_SERVO_DOOR);
+  servoDoor.write(0);
 
   servoSolarTracker.attach(PIN_SERVO_SOLAR_TRACKER);
   servoSolarTracker.write(servoSolarTrackerPos);
@@ -94,6 +140,12 @@ void setup() {
               NULL);
   xTaskCreate(TaskDoorLockController, // Task function
               "TaskDoorLockController", // Task name for humans
+              128, 
+              1000, // Task parameter
+              1, // Task priority
+              NULL);
+  xTaskCreate(TaskTempHumiReaderController, // Task function
+              "TaskTempHumiReaderController", // Task name for humans
               128, 
               1000, // Task parameter
               1, // Task priority
